1e8bc0d799a24b9b6fab6cc088059b55e7386d04
Daniel Demidko <DanielDemidko@gmail.com>
2019-08-04 19:16:07 +1000

    Update TestApplication.java

--- testapp/src/main/java/com/farpost/aot/TestApplication.java
+++ testapp/src/main/java/com/farpost/aot/TestApplication.java
@@ -8,14 +8,17 @@ import java.util.stream.Collectors;
 
 public class TestApplication {
 
-	public static void main(String[] args) throws IOException {
-		final FlexionStorage flexionStorage = new FlexionStorage();
-		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-			while (true) {
-				System.out.println(
-					Arrays.stream(flexionStorage.get(reader.readLine())).collect(Collectors.toList())
-				);
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        final FlexionStorage flexionStorage = new FlexionStorage();
+        try (final BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in))
+        ) {
+            while (true) {
+                System.out.println(Arrays.stream(
+                        flexionStorage.get(reader.readLine())
+                        ).collect(Collectors.toList())
+                );
+            }
+        }
+    }
 }
