e22cfda7ecbcab3837fd2506035d498c3cf642b8
Daniel Demidko <demidko@farpost.com>
2019-08-21 11:52:45 +1000

    кодстайл

--- compiler/src/main/java/com/farpost/aot/MiniFlexion.java
+++ compiler/src/main/java/com/farpost/aot/MiniFlexion.java
@@ -6,18 +6,18 @@ package com.farpost.aot;
 public class MiniFlexion {
 
 	private final int morphIndex;
-    private final int stringIndex;
+	private final int stringIndex;
 
 	public MiniFlexion(int stringIndex, int morphIndex) {
 		this.morphIndex = morphIndex;
-        this.stringIndex = stringIndex;
-    }
+		this.stringIndex = stringIndex;
+	}
 
-    public int getStringIndex() {
-        return stringIndex;
-    }
+	public int getStringIndex() {
+		return stringIndex;
+	}
 
 	public int getMorphIndex() {
 		return morphIndex;
-    }
+	}
 }
